03908f4ce53e832e7065443f4ebb1bc7cc50c229
ANDREI FERNANDO DA CONCEICAO <andrei.fconceicao@senacsp.edu.br>
2022-09-28 20:26:40 -0300

    Atualização de JS

--- Projeto um A+/js/script.js
+++ Projeto um A+/js/script.js
@@ -16,13 +16,44 @@ function nextImage() {
 }
 //Final do carrosel
 
-function onClickMenu () {
+function onClickMenu() {
     document.getElementById("menu").classList.toggle("icon");
     document.getElementById("nav").classList.toggle("change");
 }
 
-function trocar(){
-        
-    document.getElementById("alterar").src="./img/celhemocentro.png"
+function trocar1() {
+
+    document.getElementById("alterar").src = "./img/cellog.png"
+    document.getElementById("msg").innerText = "crie sua conta"
 }
 
+function trocar2() {
+
+    document.getElementById("alterar").src = "./img/celhemocentro.png"
+    document.getElementById("msg").innerText = "Procure no mapa um hemocentro próximo de você!"
+}
+
+
+function trocar3() {
+
+    document.getElementById("alterar").src = "./img/celhemocentro.png"
+    document.getElementById("msg").innerText = "Procure no mapa um hemocentro próximo de você!"
+}
+
+function trocar4() {
+
+    document.getElementById("alterar").src = "./img/celhemocentro.png"
+    document.getElementById("msg").innerText = "Procure no mapa um hemocentro próximo de você!"
+}
+
+function trocar5() {
+
+    document.getElementById("alterar").src = "./img/celhemocentro.png"
+    document.getElementById("msg").innerText = "Procure no mapa um hemocentro próximo de você!"
+}
+
+function trocar6() {
+
+    document.getElementById("alterar").src = "./img/celhemocentro.png"
+    document.getElementById("msg").innerText = "Procure no mapa um hemocentro próximo de você!"
+}
